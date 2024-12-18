ac1ec867dd511a094a41e34328f7cd4795a8694d
Zenon.gg <115558112+Asyraf2k@users.noreply.github.com>
2024-12-19 00:03:01 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState({ text: '', author: '' });
+  const [loading, setLoading] = useState(true);
+
+
+  const fetchQuote = async () => {
+    setLoading(true); 
+    try {
+      const response = await fetch('https://api.quotable.io/random');
+      const data = await response.json();
+      console.log('Data fetched:', data);
+      setQuote({ text: data.content, author: data.author });
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+      setLoading(false); 
+    }
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id="quote-box" className="quote-box">
+      {loading ? (
+        <div className="loading">Loading..</div> 
+      ) : (
+        <>
+          <div id="text" className="quote-text">
+            <i className="fa fa-quote-center"></i> {quote.text}
+          </div>
+          <div id="author" className="quote-author">- {quote.author}</div>
+        </>
+      )}
+      <div className="quote-buttons">
+        <button
+          id="new-quote"
+          className="btn btn-primary"
+          onClick={fetchQuote}
         >
-          Learn React
-        </a>
-      </header>
+          New Quote
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
